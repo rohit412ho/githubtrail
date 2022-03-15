0b8998b1073fb3590f2e328c5d2a0e7cf87e2ccf
hraghu05 <rhotkar5@gmail.com>
2022-03-15 23:10:44 +0530

    check

---
+++ src/newgittry1/Client.java
@@ -0,0 +1,16 @@
+package newgittry1;
+
+import java.util.Scanner;
+
+public class Client {
+
+	public static void main(String[] args) {
+		Scanner obj = new Scanner(System.in);
+		System.out.println("Enter Your Choice:");
+		
+		obj.next();
+		obj.close();
+
+	}
+
+}
